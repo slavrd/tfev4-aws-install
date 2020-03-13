8402f232a91ccbbad3c8eedfeea50413f1f7eaac
Slav Donchev <slav.donchev@gmail.com>
2020-03-13 12:52:16 +0200

    fix tf code formatting

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,14 +1,14 @@
 output "ptfe_address" {
-    value = "https://${var.ptfe_hostname}"
-    description = "Address for accessing the PTFE instance"
+  value       = "https://${var.ptfe_hostname}"
+  description = "Address for accessing the PTFE instance"
 }
 
 output "replicated_address" {
-    value = "https://${var.ptfe_hostname}:8800"
-    description = "Address for accessing the replicate console"
+  value       = "https://${var.ptfe_hostname}:8800"
+  description = "Address for accessing the replicate console"
 }
 
 output "private_key" {
-    value = var.key_pair_create ? module.key_pair.private_key : ""
-    description = "The private key of the TLS key pair if such was created."
+  value       = var.key_pair_create ? module.key_pair.private_key : ""
+  description = "The private key of the TLS key pair if such was created."
 }
