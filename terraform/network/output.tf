28ae65a2d7e37e27574d8e7c2ae275ea82823d44
Slav Donchev <slav.donchev@gmail.com>
2020-03-14 17:10:32 +0200

    fix netowrk target group outputs

--- terraform/network/output.tf
+++ terraform/network/output.tf
@@ -19,16 +19,16 @@ output "lb_dns_name" {
 }
 
 output "lb_tg_80_arn" {
-  value       = aws_lb.ptfe.dns_name
+  value       = aws_lb_target_group.port_80.arn
   description = "ARN (id) of the load balancer target group assigned to the listner on port 80."
 }
 
 output "lb_tg_443_arn" {
-  value       = aws_lb.ptfe.dns_name
+  value       = aws_lb_target_group.port_443.arn
   description = "ARN (id) of the load balancer target group assigned to the listner on port 443."
 }
 
 output "lb_tg_8800_arn" {
-  value       = aws_lb.ptfe.dns_name
+  value       = aws_lb_target_group.port_8800.arn
   description = "ARN (id) of the load balancer target group assigned to the listner on port 8800."
 }
