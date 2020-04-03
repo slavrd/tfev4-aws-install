bb9f60798a1d9245dbfb952130ec31236bfcb743
Slav Donchev <slav.donchev@gmail.com>
2020-04-03 07:35:41 +0300

    add asset download script

---
+++ packer/scripts/setup_download_replicated.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Dwonloads replicated installer and ptfe airgap package
+
+[ -z $PTFE_AIRGAP_URL ] && {
+    echo "==> PTFE_AIRGAP_URL not set"
+    exit 1
+}
+[ -z $REPLICATED_VER ] && {
+    REPLICATED_INSTALLER_URL='https://s3.amazonaws.com/replicated-airgap-work/replicated.tar.gz'
+} || {
+    REPLICATED_INSTALLER_URL="https://s3.amazonaws.com/replicated-airgap-work/stable/replicated-${REPLICATED_VER}%2B${REPLICATED_VER}%2B${REPLICATED_VER}.tar.gz"
+}
+
+echo "==> downloading PTFE airgap package from ${PTFE_AIRGAP_URL}"
+wget -O /tmp/terraform-enterprise.airgap $PTFE_AIRGAP_URL || echo "filed downloading PTFE airgap package"; exit 1
+
+echo "==> downloading Replicated installer from ${REPLICATED_INSTALLER_URL}"
+wget -O /tmp/replicated.tar.gz $REPLICATED_INSTALLER_URL || echo "filed downloading Replicated installer package"; exit 1
