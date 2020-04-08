aa75b290b1c260d10155dd90dd8e317fd5c86737
Slav Donchev <slav.donchev@gmail.com>
2020-04-08 17:36:03 +0300

    remove unused ec-instance terraform module

--- terraform/ec2-instance/templates/install_wrap.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-private_ip=$(curl -sSf 'http://169.254.169.254/latest/meta-data/local-ipv4')
-[ -z "$private_ip" ] && exit 1
-public_ip=$(curl -sSf 'http://169.254.169.254/latest/meta-data/public-ipv4')
-[ -z "$public_ip" ] && public_ip=${private_ip}
-./install.sh airgap no-proxy private-address=${private_ip} public-address=${public_ip}
