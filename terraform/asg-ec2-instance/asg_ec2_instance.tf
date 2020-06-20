54479a9e88587014c46bf651736f32ca1937befb
Slav Donchev <slav.donchev@gmail.com>
2020-06-20 22:21:40 +0300

    add health_check_type input variable to asg module

--- terraform/asg-ec2-instance/asg_ec2_instance.tf
+++ terraform/asg-ec2-instance/asg_ec2_instance.tf
@@ -44,7 +44,7 @@ resource "aws_autoscaling_group" "tfe" {
   max_size                  = 1
   min_size                  = 1
   health_check_grace_period = 300
-  health_check_type         = "ELB"
+  health_check_type         = var.health_check_type
   launch_configuration      = aws_launch_configuration.tfe.name
   vpc_zone_identifier       = var.subnets_ids
   target_group_arns         = var.target_groups_arns
